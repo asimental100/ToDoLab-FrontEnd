6214e1cc5a9b504474b79f02e44c129fca994ad8
Adrian Simental <asimental100@hotmail.com>
2020-08-19 15:49:08 -0700

    basic framework 3

---
+++ src/Logic.js
@@ -0,0 +1,41 @@
+  
+import React, { Component } from 'react'
+
+export default class LogIn extends Component {
+    state = {
+        email: '',
+        password: '',
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const token = Math.random();
+
+        console.log('=============================\n')
+        console.log('|| token', token, this.state.email, this.state.password)
+        console.log('\n=============================')
+
+        this.props.handleTokenChange(token);
+        this.props.history.push('/todos');
+    }
+    render() {
+        return (
+            <div>
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    email
+                    <input onChange={(e) => this.setState({ email: e.target.value })} value={this.state.email} />
+                </label>
+                <br/>
+                <label>
+                    password
+                    <input onChange={(e) => this.setState({ password: e.target.value })} value={this.state.password} />
+                </label>
+                <br/>
+                <button>log in</button>
+            </form>
+            </div>
+        )
+    }
+}
